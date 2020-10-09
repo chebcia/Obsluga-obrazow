e4657f4277f470f402c8d4b98f84f000c601ddcc
Natalia Cheba <43813992+chebcia@users.noreply.github.com>
2020-10-09 18:39:23 +0200

    Add files via upload
    -stworzenie połączenia C++ do C#
    -początek interfejsu

---
+++ Projekt_JA/rozmycie/GUI/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.image1 = new System.Windows.Forms.PictureBox();
+            this.opbl = new System.Windows.Forms.OpenFileDialog();
+            this.pbpic = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbpic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.AliceBlue;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(209, 77);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Przeglądaj...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(337, 77);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 32);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(727, 267);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // image1
+            // 
+            this.image1.Location = new System.Drawing.Point(653, 47);
+            this.image1.Name = "image1";
+            this.image1.Size = new System.Drawing.Size(245, 189);
+            this.image1.TabIndex = 4;
+            this.image1.TabStop = false;
+            // 
+            // opbl
+            // 
+            this.opbl.FileName = "opbl";
+            // 
+            // pbpic
+            // 
+            this.pbpic.Location = new System.Drawing.Point(66, 135);
+            this.pbpic.Name = "pbpic";
+            this.pbpic.Size = new System.Drawing.Size(165, 86);
+            this.pbpic.TabIndex = 5;
+            this.pbpic.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(96, 227);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 41);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button3";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.BackgroundImage = global::GUI.Properties.Resources.s4_konkurs_fotograficzny_brodnica___moje_miasto_a_w_nim_1570637800_2459;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(942, 678);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pbpic);
+            this.Controls.Add(this.image1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Konwerter obrazu";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbpic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox image1;
+        private System.Windows.Forms.OpenFileDialog opbl;
+        private System.Windows.Forms.PictureBox pbpic;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Projekt_JA/rozmycie/GUI/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("rozmycie.dll")]          //w jakiej ddl bede wyszukiwała symbolu
+        private static extern int sprawdz(int x); //
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openimage();
+            import();
+            
+        }
+
+        private Bitmap import()
+        {
+
+            Bitmap copy;
+            using (var image = new Bitmap(@"img.jpg"))
+            {
+                copy = new Bitmap(image);
+            }
+            return copy;
+
+        }
+        //rozbudowana wersja wzieta z neta równiez sie nie odpala
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("An Error occured", "Eror", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            string imageLocation = "";
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(.*jpg)|*.jpg| PNG files(.*png)|*.png| All Files(*.*)|*.*";
+
+                if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName;
+                    image1.ImageLocation = imageLocation;
+                }
+
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("An Error occured", "Eror", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        //wziete z neta nie działa, tzn nie odpala sie ten przycisk
+        private void button5_Click(object sender, EventArgs e)
+        {
+          opbl.Filter = "jpg|*.jpg";
+
+            DialogResult res = opbl.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                pbpic.Image = Image.FromFile(opbl.FileName);
+
+            }
+        }
+
+
+
+
+
+        private Bitmap openimage()
+        {
+
+            Bitmap copy;
+            using (var image = new Bitmap(@"img.jpg"))
+            {
+                copy = new Bitmap(image);
+            }
+            return copy;
+
+        }
+        public byte[] ImageToByteArray(System.Drawing.Image image)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, image.RawFormat);
+                return ms.ToArray();
+            }
+        }
+
+        
+
+
+    }
+
+}
